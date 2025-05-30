ea088ab45b18793831ce528dfb7528526b44b779
Leaaooo <leaadeliee77@gmail.com>
2025-05-30 21:09:35 +0700

    Fix: update import to character.css (lowercase)

--- src/pages/character.jsx
+++ src/pages/character.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./Character.css";
+import "./character.css";
 
 const characters = [
   { id: 'nutto', frame: '/assets/nutto.png', image: '/assets/tupai.png' },
